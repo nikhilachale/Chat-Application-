5cbbe065d3ba003e9c4befbbc4f5e41b65c70d92
nikhil achale <nicachale456@gmail.com>
2025-03-04 13:00:05 +0530

    folder updated

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,20 +1,20 @@
 
-import { WebSocketServer } from "ws";
-
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 })
-
-let uc = 0;
-
-let allsockets = [];
-wss.on("connection", (socket) => {
+let allsockets: WebSocket[] = [];
+wss.on("connection", (socket: WebSocket) => {
     allsockets.push(socket)
-    console.log("user created : ", + ++uc);
+    console.log("user created : ");
     socket.on("message", (event) => {
         console.log("message recived:" + event.toString())
         for (let i = 0; i < allsockets.length; i++) {
             const s=allsockets[i];
             s.send(event.toString() + "sent from :" +s)
         }
+    })
+
+    socket.on("disconnect",()=>{
+        allsockets=allsockets.filter(x=>x!=socket)
     })
 
 })
